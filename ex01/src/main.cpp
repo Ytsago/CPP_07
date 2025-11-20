966ee97a2770202d6ad7538b5381b2a7d20efedc
Sebastien Cros <secros@z3r4p2.42lyon.fr>
2025-11-20 14:14:49 +0100

    Better ex01 test main

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -10,10 +10,23 @@ void	addFour(int& x) {
 }
 
 int main() {
+	std::ostream &out = std::cout;
 	const std::string someText = "Some text is here ";
-	const int i[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int i[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 
-	iter(i, 8, addFour);
+	out << "Using print<T>(const T& c); with int[8]...\n";
 	iter(i, 8, print<int>);
+	out << "\n\nAdding 4 to each element of int[8]...\n";
+	iter(i, 8, addFour);
+	out << "New values...\n";
+	iter(i, 8, print<int>);
+
+	const int i2[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+	out << "\n\nUsing print<T>(const T& c); with const int[8]...\n";
+	iter(i2, 8, print<const int>);
+
+	out << "\n\nUsing print<T>(const T& c); with const std::string...\n";
 	iter(someText.c_str(), someText.length(), print<char>);
+
+
 }
